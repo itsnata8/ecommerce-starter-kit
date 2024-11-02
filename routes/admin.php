f081ff5b35873b31ccfbf8e92bba54014c892c4e
nata <rizqinata8@gmail.com>
2024-11-02 10:18:09 +0700

    feat: admin auth login and logout

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-Route::get('/admin', function () {
-    return 'hey admin';
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::middleware(['guest:admin'])->group(function () {
+        Route::view('/login', 'back.pages.admin.auth.login')->name('login');
+        Route::post('/login_handler', [AdminController::class, 'login_handler'])->name('login_handler');
+    });
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::view('/home', 'back.pages.admin.home')->name('home');
+        Route::post('/logout_handler', [AdminController::class, 'logoutHandler'])->name('logout_handler');
+    });
 });
